5cb89fd967918ba263414016640c412e1a91f1d8
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-01-28 11:07:57 -0600

    docs:clean up code per PEP8online

--- database_setup.py
+++ database_setup.py
@@ -10,7 +10,11 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-# TODO:user:restaurant is one: many; user:condition is one:many or many:many?; user:menu is there relationship?
+
+# Here I define table 'user' and its mapped class User
+# user:restaurant is one: many; see lines 24,41.
+# user:condition is many:many;
+# user:menu no direct relationship
 class User(Base):
     __tablename__ = 'user'
 
@@ -18,9 +22,9 @@ class User(Base):
     name = Column(String(25))
     email = Column(String(30))
     picture = Column(String(30))
-    restaurants = relationship('Restaurant', back_populates='user') # TODO:more rels
+    restaurants = relationship('Restaurant', back_populates='user')
     conditions = relationship('Condition', back_populates='user')
-    menus = relationship('MenuItem',back_populates='user')
+    # menus = relationship('MenuItem',back_populates='user')
 
 
 # restaurant:menu  is one:many;condition:menu is many:many;
@@ -31,9 +35,11 @@ class Restaurant(Base):
     # column 'name' is mapped to the 'name' attribute of class 'Restaurant'
     name = Column(String(250), nullable=False)
     description = Column(String(250))
-    menus = relationship('MenuItem',back_populates='restaurant')
+    menus = relationship('MenuItem', back_populates='restaurant')
+    # ForeignKey construct, builds RELATION (bidirectional)
+    # 'user_id' value is constrained to that in the 'user.id' column
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship('User',back_populates='restaurants')
+    user = relationship('User', back_populates='restaurants')
 
     def __repr__(self):
         return "<Restaurant(name='%s', description='%s')>" % (
@@ -41,16 +47,16 @@ class Restaurant(Base):
 
     @property
     def serialize(self):
-        return {'name':self.name,
-                'description':self.description,
-                'id':self.id,
-                'user_id':self.user_id
+        return {'name': self.name,
+                'description': self.description,
+                'id': self.id,
+                'user_id': self.user_id
                 }
 
-condition_menu = Table('condition_menu',Base.metadata,
-                          Column('condition_id', Integer, ForeignKey('condition.id')),
-                          Column('menu_id', Integer, ForeignKey('menu_item.id'))
-                          )
+condition_menu = Table(
+    'condition_menu', Base.metadata,
+    Column('condition_id', Integer, ForeignKey('condition.id')),
+    Column('menu_id', Integer, ForeignKey('menu_item.id')))
 
 
 class Condition(Base):
@@ -59,9 +65,11 @@ class Condition(Base):
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     signs_and_symptoms = Column(String(250))
-    suggested_menus = relationship('MenuItem',secondary=condition_menu, back_populates='conditions')
+    suggested_menus = relationship('MenuItem', secondary=condition_menu,
+                                   back_populates='conditions')
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship('User', back_populates='conditions')
+
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
@@ -73,24 +81,28 @@ class MenuItem(Base):
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship('Restaurant', back_populates='menus')
-    conditions = relationship('Condition', secondary=condition_menu, back_populates='suggested_menus')
+    conditions = relationship(
+        'Condition', secondary=condition_menu,
+        back_populates='suggested_menus')
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship('User', back_populates='menus')
 
-# Restaurant.menu_item = relationship('MenuItem',order_by=MenuItem.id,back_populates='restaurant')
+# Restaurant.menu_item = relationship('MenuItem', order_by=MenuItem.id,
+# back_populates='restaurant')
 
     @property
     def serialize(self):
-        return {'name':self.name,
-                'course':self.course,
-                'description':self.description,
-                'price':self.price,
-                'id':self.id,
-                'restaurant_id':self.restaurant_id,
-             #    'condition_id':self.condition_id
-             #   'restaurant':self.restaurant  # 'restaurant is not serializable'
+        return {'name': self.name,
+                'course': self.course,
+                'description': self.description,
+                'price': self.price,
+                'id': self.id,
+                'restaurant_id': self.restaurant_id,
+                # 'restaurant':self.restaurant:restaurant is not serializable
                 }
 
-# issue CREATE statements for all tables using MetaData object created during declarative_base()
+# create an instance of Engine to be connected to SQLite database
+# issue CREATE statements for all tables using MetaData object created during
+# declarative_base()
 engine = create_engine('sqlite:///restaurantmenuconditionuser.db', echo=True)
 Base.metadata.create_all(engine)
